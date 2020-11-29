564b158510b4d5e0205e0fa6a88cc7fe03705c01
joshua <josuasirustara@gmail.com>
2020-11-29 16:19:57 +0700

    27/11/2020

---
+++ ujian1/Controller/Controller.h
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "../Models/Models.h"
+
+int parseInt(char *month){
+    if(!strcmp(month,"january")){
+        return 1;
+    }else if(!strcmp(month,"february")){
+        return 2;
+    }else if(!strcmp(month,"march")){
+        return 3;
+    }else if(!strcmp(month,"april")){
+        return 4;
+    }else if(!strcmp(month,"may")){
+        return 5;
+    }else if(!strcmp(month,"june")){
+        return 6;
+    }else if(!strcmp(month,"july")){
+        return 7;
+    }else if(!strcmp(month,"august")){
+        return 8;
+    }else if(!strcmp(month,"september")){
+        return 9;
+    }else if(!strcmp(month,"november")){
+        return 11;
+    }else if(!strcmp(month,"december")){
+        return 12;
+    }else if(!strcmp(month,"ocotber")){
+       
+        return 10;
+    }else{
+        return 0;
+    }}
+link *createP(patient a){
+     link *newP=(link *)malloc(sizeof(link));
+    strcpy(newP->zero.name,a.name);
+     strcpy(newP->zero.vmonth,a.vmonth);
+    newP->zero.date=a.date;
+    newP->zero.year=a.year;
+    newP->zero.month=parseInt(a.vmonth);
+    newP->next=newP->prev=NULL;
+    return newP;
+}
+
+link *push(patient a,link *curr){
+    link *newP=createP(a),*temp=curr;
+    if(curr){
+        while(curr->next&&(a.year>curr->zero.year||(a.year==curr->zero.year&&a.month>curr->zero.month)||(a.year==curr->zero.year&&a.month==curr->zero.month&&a.date>curr->zero.date))){
+            curr=curr->next;
+        }
+        if(!curr->next&&(a.year>curr->zero.year||(a.year==curr->zero.year&&a.month>curr->zero.month)||(a.year==curr->zero.year&&a.month==curr->zero.month&&a.date>curr->zero.date))){
+        	curr->next=newP;
+            newP->prev=curr; 
+		}else if(!curr->prev){
+            newP->next=curr;
+            curr->prev=newP;
+             return newP;
+        }else{
+             newP->next=curr;
+             newP->prev=curr->prev;
+            curr->prev->next=newP;
+            curr->prev=newP;
+        }
+        return temp;
+    }else{
+        curr=newP;
+        return curr;
+    }
+}
+link *pop(link *curr){
+    link *temp=curr;
+    curr=curr->next;
+    if(curr){
+        curr->prev=NULL;
+    }
+    free(temp);
+    temp=NULL;
+    return curr;
+}
+void printAll(link *curr){
+    while(curr){
+        printf("%d %s %d - %s\n",curr->zero.date,curr->zero.vmonth,curr->zero.year,curr->zero.name);
+        curr=curr->next;
+    }
+}
+
---
+++ ujian1/View/View.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "../Controller/Controller.h"
+
+int main(){
+    int cureT,patientT;
+    link *head=NULL;
+   patient toAdd;
+    scanf("%d %d",&patientT,&cureT);
+    getchar();
+   while(patientT--){
+       do{
+        scanf("%d %s %d - %[^\n]",&toAdd.date,toAdd.vmonth,&toAdd.year,toAdd.name);
+        getchar();}while(!parseInt(toAdd.vmonth));
+        head=push(toAdd,head);
+    }
+    while(head&&cureT--){
+        head=pop(head);
+    }
+    if(!head){
+        printf("All patients get the cure, %d cure left\n",cureT);
+    }else{
+        printf("Need %d more cure\n",patientT);
+        printAll(head);
+    }
+    while(head){
+        head=pop(head);
+    }
+    return 0;
+}
